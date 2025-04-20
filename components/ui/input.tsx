42ac03c56c4cbd72660ceaea217f481598d08049
rajofearth <rajofearth@proton.me>
2025-04-20 09:36:59 +0530

    Fix: Support custom size prop by omitting native size attribute in Input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-type InputProps = React.ComponentProps<"input"> & {
+type InputProps = Omit<React.ComponentProps<"input">, "size"> & {
   size?: "default" | "sm" | "lg"
 }
 
